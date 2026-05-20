70c66fa88b063d6c73f85af5234dfb2df458f394
Danila Iakovlev <moleculadesigner@gmail.com>
2026-05-20 14:00:17 +0300

    ADD Two body gravity calculation

---
+++ sim/include/two-body-gravity.hpp
@@ -0,0 +1,28 @@
+#ifndef TWO_BODY_GRAVITY
+#define TWO_BODY_GRAVITY
+#include <array>
+
+namespace gravity {
+static double G = 6.675542e-11;
+class Body{
+    private:
+        double x, y, z;
+        double m;
+        
+    
+    public:
+        Body(double x, double y, double z, double m): x(x), y(y), z(z), m(m) {}
+        Body(std::array<double, 3> &coords, double mass);
+        
+        // Access
+        std::array<double, 3> X();
+        double M();
+  
+        // Measure
+        double sq_dist_to(Body &other);
+}; // Body
+
+double gravity_force(Body &b1, Body &b2);
+} // gravity      
+
+#endif // TWO_BODY_GRAVITY
---
+++ sim/src/two-body-gravity.cpp
@@ -0,0 +1,35 @@
+#include "two-body-gravity.hpp"
+#include <cmath>
+#include <array>
+
+namespace gravity {
+Body::Body(std::array<double, 3> &coords, double mass) {
+    this->x = coords[0];
+    this->y = coords[1];
+    this->z = coords[2];
+    this->m = mass;
+}
+
+std::array<double, 3> Body::X() {
+    return std::array<double, 3>{
+        this->x,
+        this->y,
+        this->z
+    };
+}
+
+double Body::M() {
+    return this->m;
+}
+    
+double Body::sq_dist_to(Body &other) {
+    return std::pow(this->x - other.x, 2)
+         + std::pow(this->y - other.y, 2)
+         + std::pow(this->z - other.z, 2);
+}
+
+double gravity_force(Body &b1, Body &b2) {
+    return G * b1.M() * b2.M() / b1.sq_dist_to(b2);
+}
+
+} // namespace gravity
